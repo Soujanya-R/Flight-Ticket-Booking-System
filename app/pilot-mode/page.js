f7c8fd6e8752219651082e5806209cb7f2f80135
Soujanya-R <soujanyaravi26@gmail.com>
2025-03-07 23:11:55 +0530

    Added seat booking functionality

---
+++ app/pilot-mode/page.js
@@ -0,0 +1,10 @@
+import PilotMode from "@/components/PilotMode";
+
+export default function FlightSimPage() {
+  return (
+    <div>
+      <h1 className="text-center text-2xl font-bold p-4">Pilot Mode</h1>
+      <PilotMode />
+    </div>
+  );
+}
